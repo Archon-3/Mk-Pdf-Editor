6084ab7188d1b7462a6b0284930bc5827ce14928
Mk Pdf Editor <mkpdfeditor@example.com>
2026-09-03 09:45:33 +0300

    corrrecting the packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8000',
+    },
+  },
   test: {
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
